3bb4e05e5d5972106a236f3305bd0af9405859f8
Baris <baris.devrim1@gmail.com>
2022-03-29 13:47:43 +0300

    Update Header.js
    Image storage address is relocated.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <Row className='mt-5 px-3 '>
             <Card className='HeaderCard pt-3 d-flex align-items-center justify-content-center '>
-                <Card.Img variant="top" src="http://localhost:3000/assets/profilePhoto.jpeg" />
+                <Card.Img variant="top" src="/assets/profilePhoto.jpeg" />
                 <Card.Body className='d-flex flex-column justify-content-center align-items-center w-100'>
                     <Card.Title as="h1" className='headerCardTitle text-center'> Spend Baris' Money </Card.Title>
                 </Card.Body>
